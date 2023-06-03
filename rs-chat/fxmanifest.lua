51c12a0803a95ef4724458fa9e1b56ebc47e046e
Rare Services <134977201+RareServices@users.noreply.github.com>
2023-06-03 22:40:54 +0300

    WORKING Update fxmanifest.lua

--- rs-chat/fxmanifest.lua
+++ rs-chat/fxmanifest.lua
@@ -4,25 +4,27 @@ lua54 'yes'
 
 author 'Rare Services'
 description 'Chat System With Reply And Automessage for ESX 1.1 and Highter'
-
 version '1.2.2'
 
-shared_scripts 'config.lua'
+shared_scripts 'source/config.lua'
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'source/server.lua'
+    '@mysql-async/lib/MySQL.lua',
+    'source/config.lua',
+    'source/server.lua'
 }
 
-client_scripts 'source/client.lua'
-
+client_scripts  {
+     'source/config.lua',
+     'source/client.lua'
+}
 
 files {
-	'ui/ui.html',
-	'ui/js/*.js',
-	'ui/css/*.css',
-	'ui/images/icons/*.png',
-	'ui/css/fonts/*.ttf'
+    'ui/ui.html',
+    'ui/js/*.js',
+    'ui/css/*.css',
+    'ui/images/icons/*.png',
+    'ui/css/fonts/*.ttf'
 }
 
 ui_page 'ui/ui.html'
